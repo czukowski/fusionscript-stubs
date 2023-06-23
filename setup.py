9d102a4e97895bb7f47e72f61b67b94b50bc0f60
Korney Czukowski <carbofos@seznam.cz>
2023-06-23 17:07:31 +0200

    Package description for PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup
+from pathlib import Path
 
 
 setup(
     name="fusionscript-stubs",
     url="https://github.com/czukowski/fusionscript-stubs",
+    description="DaVinci Resolve Python API stubs",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
     author="Korney Czukowski",
     author_email="carbofos@seznam.cz",
-    version="18.1.2",
+    version="18.1.3",
     package_data={"fusionscript-stubs": ['fusionscript.pyi', '__init__.pyi']},
     packages=["fusionscript-stubs"]
 )
